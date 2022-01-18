86797dd6ba541ba46f26d5a021fa114c05a42b4a
Tyler Adam Martinez <tyleradammartinez@dynamic-10-125-191-122.frisco.wireless.unt.edu>
2022-01-18 13:28:23 -0600

    fix: rm unnesscessary dependencies

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import './App.css';
 import requests from './request';
 import NavBar from './NavBar';
--- src/NavBar.js
+++ src/NavBar.js
@@ -9,9 +9,22 @@ const NavBar = () => {
       {id: 1, element: "TV Shows"},
       {id: 2, element: "Movies"},
     ];
-  
+
+    const [backgroundClasses, setBackgroundClasses] = useState("navBar no-background");
+    useEffect(() => {
+        let lastKnownScrollPosition;
+        document.addEventListener('scroll', () => {
+            lastKnownScrollPosition = window.scrollY;
+            if(lastKnownScrollPosition < 30) {
+                setBackgroundClasses("navBar no-background-color");
+            } else {
+                setBackgroundClasses("navBar background-color");
+            }
+        });
+    }, []);
+
     return (
-      <nav className='navBar no-background'>
+      <nav className={backgroundClasses}>
         <img src={Netflix_Title_Banner} alt='Netflix Banner Logo' height={100} width={100}/>
         <ul>
           { navBarList.map((listItem) => (<li key={listItem.id}>{listItem.element}</li>)) }
